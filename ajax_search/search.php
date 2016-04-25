2068e496e25973e1791dbff1d6750ac5c45b98c5
codesboy <565195693@qq.com>
2016-04-25 22:16:34 +0800

    fetch_assoc

--- ajax_search/search.php
+++ ajax_search/search.php
@@ -6,29 +6,26 @@
  */
 header("Content-type:text/html;charset=utf-8");
 
-include_once('config.php');
+require_once 'config.php';
 
 
 
 $search=$_POST['search'];
+// $search='房';
 $searchSql="select * from data where note like '%{$search}%'";
 
 $searchResult=$con->query($searchSql);
 
-while($searchArr=$searchResult->fetch_array()){//遍历结果集的数组
-
-    foreach ($searchArr as $key => $value) {
-        $searchArr[$key]=$value;
-    }
-
-    $arr[]=$searchArr;
+while($row=$searchResult->fetch_assoc()){//从结果集中取出每一行进行循环
+    // var_dump($row);
+    $arr[]=$row;
 }
-
+/*echo "<pre>";
+print_r($arr);
+die;*/
 if($search){
     echo json_encode($arr,JSON_UNESCAPED_UNICODE);
 }
-
-
 
 $searchResult->free();//释放结果集
 $con->close();//关闭数据库连接
